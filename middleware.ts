9ecf9e74d31cfcbe06494ae8d0d3dc751721cca2
ciello victor <ciellolisboa023@gmail.com>
2026-04-11 00:09:03 -0300

    fix: export default auth for middleware to prevent Vercel NFT error

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/lib/auth-edge";
 import { NextResponse } from "next/server";
 
-export const middleware = auth((req) => {
+export default auth((req) => {
   const { pathname } = req.nextUrl;
   const session = req.auth;
 
